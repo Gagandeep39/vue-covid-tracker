a3f87be176ddea4ba80aeb5bb26451579f7cee29
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-23 19:43:54 +0000

    Added loading UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,13 @@
   <main v-if="!loading" class="home">
     Show Data
   </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching data...
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
