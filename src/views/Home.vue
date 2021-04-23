51143c4b883e3cafeb4ead27a2e798a6fa307be4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-23 19:35:08 +0000

    Added api data fetch logic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,38 @@
 <template>
-  <div class="home">
-    Hell World
-  </div>
+  <main v-if="!loading" class="home">
+    Show Data
+  </main>
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {},
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.countries;
+    this.loading = false;
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif'),
+    };
+  },
 };
 </script>
 
